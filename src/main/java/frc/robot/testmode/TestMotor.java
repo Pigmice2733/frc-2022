d3533253496dabd9e6f3f2e6107af6962acd651f
var Cepheid <avhoi83@gmail.com>
2022-01-21 17:33:06 -0800

    added a second motor for the shooter

--- src/main/java/frc/robot/testmode/TestMotor.java
+++ src/main/java/frc/robot/testmode/TestMotor.java
@@ -5,12 +5,14 @@ import edu.wpi.first.wpilibj.SpeedController;
 
 public class TestMotor extends TestBase {
 
-  private final SpeedController motor;
+  private final SpeedController forwardMotor;
+  private final SpeedController reverseMotor;
   private final Encoder encoder;
   private final boolean positiveIsForward;
 
-  public TestMotor(SpeedController motor, Encoder encoder, boolean positiveIsForward) {
-    this.motor = motor;
+  public TestMotor(SpeedController lowerWheelMotor, SpeedController upperWheelMotor, Encoder encoder, boolean positiveIsForward) {
+    this.forwardMotor = lowerWheelMotor;
+    this.reverseMotor = upperWheelMotor;
     this.encoder = encoder;
     this.positiveIsForward = positiveIsForward;
   }
@@ -23,13 +25,13 @@ public class TestMotor extends TestBase {
     if (!positiveIsForward) {
       speed*=-1;
     }
-    motor.set(speed);
+    forwardMotor.set(speed);
     try {
       Thread.sleep(200);
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    motor.set(0.0);
+    forwardMotor.set(0.0);
     int stopEncoder = encoder.get();
     if (forward) {
       return stopEncoder > startEncoder;
