2a3629fb14b93365873fb35e44eb20f5bf8acc0d
ev118 <evenn118@gmail.com>
2022-01-21 16:07:28 -0800

    minor changes

--- src/main/java/frc/robot/subsystems/RandomMotor.java
+++ src/main/java/frc/robot/subsystems/RandomMotor.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.sensors.CANCoder;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -38,10 +37,10 @@ public class RandomMotor extends SubsystemBase {
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
-        double output = 1.0;
+        double output = 0.3;
         motor.set(ControlMode.PercentOutput, output);
         double velocity = motor.getSelectedSensorVelocity(); // can only be run once per tick?
-        SmartDashboard.putNumber("Motor Velocity", calculateRPM(velocity, output));
+        SmartDashboard.putNumber("Motor Velocity (rpm)", calculateRPM(velocity, output));
         SmartDashboard.putNumber("Raw Velocity", velocity);
     }
 
