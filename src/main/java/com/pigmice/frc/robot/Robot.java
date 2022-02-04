af41d2161aa7b83c650231b64d69c113dee2810d
PepperLola <julian.orion.wright@gmail.com>
2022-02-03 20:27:35 -0800

    removed start and stop subsystem calls from Robot.java

--- src/main/java/com/pigmice/frc/robot/Robot.java
+++ src/main/java/com/pigmice/frc/robot/Robot.java
@@ -64,7 +64,7 @@ public class Robot extends TimedRobot {
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
-    m_robotContainer.disableSubsystems();
+
   }
 
   @Override
@@ -99,7 +99,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-    m_robotContainer.enableSubsystems();
   }
 
   /** This function is called periodically during operator control. */
