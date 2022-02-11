e051b93528514d81bca1ace4f4054d38c2043f83
nev3rofficial <mayafmayaf@icloud.com>
2022-02-11 15:59:19 -0800

    fixed BallTracker::getCompartment

--- src/main/java/com/pigmice/frc/robot/BallTracker.java
+++ src/main/java/com/pigmice/frc/robot/BallTracker.java
@@ -4,33 +4,31 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 //this here class is to find where yo ballz at
-public class BallTracker{
+public class BallTracker {
 
     Queue<BallColor> balls = new LinkedList<>();
-    
-    //set the first position to a color and shift everything else up
-    public void newBallStored(BallColor color){
+
+    // set the first position to a color and shift everything else up
+    public void newBallStored(BallColor color) {
         balls.add(color);
     }
 
-    //get rid of the second position and shift everything else up
-    public void ballLaunched(){
+    // get rid of the second position and shift everything else up
+    public void ballLaunched() {
         balls.remove();
     }
 
-    //return the color of ball in a spot. 0 is the next one to be shot, 1 is the second one to be shot 
-    public String getCompartment(int spotNumber){
-        if(number == 0)
-        {
+    // return the color of ball in a spot. 0 is the next one to be shot, 1 is the
+    // second one to be shot
+    public BallColor getCompartment(int spotNumber) {
+        if (spotNumber == 0) {
             return balls.peek();
-        }
-        else if(number == 1)   
-        {
+        } else {
             return balls.element();
         }
     }
 
     enum BallColor {
-        RED,BLUE,NONE
+        RED, BLUE, NONE
     }
 }
