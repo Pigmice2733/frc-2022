798a8495f9ad6f9217218236df34bedc512bcb10
ev118 <evenn118@gmail.com>
2022-02-14 17:02:28 -0800

    minor changes to Drivetrain

--- src/main/java/com/pigmice/frc/robot/Constants.java
+++ src/main/java/com/pigmice/frc/robot/Constants.java
@@ -9,15 +9,13 @@ import edu.wpi.first.wpilibj.SPI.Port;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
  * <p>
  * It is advised to statically import this class (or one of its inner classes)
- * wherever the
- * constants are needed, to reduce verbosity.
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
     public static class ShooterConfig {
@@ -43,6 +41,13 @@ public final class Constants {
         public static final double wheelBase = 0.603;
 
         public static final int navXRotationalOffsetDegrees = 0;
+    }
+
+    public static class IntakeConfig {
+        public static final int intakeBottomPort = 0;
+        public static final int intakeTopPort = 1;
+
+        public static final double intakeSpeed = 0.75;
     }
 
     public static final int driverControllerPort = 0;
