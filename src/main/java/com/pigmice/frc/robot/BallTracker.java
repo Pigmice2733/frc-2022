165597e60798c054f559047f23c78478f0efacbc
var Cepheid <avhoi83@gmail.com>
2022-02-14 16:39:04 -0800

    replaced the queue implementation of the ball tracker with an array

--- src/main/java/com/pigmice/frc/robot/BallTracker.java
+++ src/main/java/com/pigmice/frc/robot/BallTracker.java
@@ -1,34 +1,71 @@
 package com.pigmice.frc.robot;
 
-import java.util.LinkedList;
-import java.util.Queue;
+/* import java.util.LinkedList;
+import java.util.Queue; */
 
-//this here class is to find where yo ballz at
-public class BallTracker{
-/*
-    Queue<BallColor> balls = new LinkedList<>();
-
-    // set the first position to a color and shift everything else up
-    public void newBallStored(BallColor color) {
-        balls.add(color);
+/** this here class is to find where yo ballz at */
+public class BallTracker {
+    /*
+     * Queue<BallColor> balls = new LinkedList<>();
+     * 
+     * // set the first position to a color and shift everything else up
+     * public void newBallStored(BallColor color) {
+     * balls.add(color);
+     * }
+     * 
+     * // get rid of the second position and shift everything else up
+     * public void ballLaunched() {
+     * balls.remove();
+     * }
+     * 
+     * // return the color of ball in a spot. 0 is the next one to be shot, 1 is the
+     * // second one to be shot
+     * public BallColor getCompartment(int spotNumber) {
+     * if (spotNumber == 0) {
+     * return balls.peek();
+     * } else {
+     * return balls.element();
+     * }
+     * }
+     * 
+     * enum BallColor {
+     * RED,BLUE,NONE
+     * }
+     */
+    enum Colors {
+        RED, BLUE, NONE
     }
 
-    // get rid of the second position and shift everything else up
-    public void ballLaunched() {
-        balls.remove();
+    Colors[] balls = { Colors.NONE, Colors.NONE };
+
+    public boolean isEmpty() {
+        if (balls[0] == Colors.NONE && balls[1] == Colors.NONE) {
+            return true;
+        }
+        return false;
     }
 
-    // return the color of ball in a spot. 0 is the next one to be shot, 1 is the
-    // second one to be shot
-    public BallColor getCompartment(int spotNumber) {
-        if (spotNumber == 0) {
-            return balls.peek();
+    public boolean isFull() {
+        if (balls[0] == Colors.NONE || balls[1] == Colors.NONE) {
+            return false;
+        }
+        return true;
+    }
+
+    public void newBall(Colors color) {
+        if (isEmpty()) {
+            balls[0] = color;
         } else {
-            return balls.element();
+            balls[1] = color;
         }
     }
 
-    enum BallColor {
-        RED,BLUE,NONE
-    }*/
+    public void ballLaunched() {
+        balls[0] = balls[1];
+        balls[1] = Colors.NONE;
+    }
+
+    public Colors getColor(int spot) {
+        return balls[spot];
+    }
 }
