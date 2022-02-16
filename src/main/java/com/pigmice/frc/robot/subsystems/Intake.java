5863d352cc096c306d8f966de97ff7afb2e7fafb
ElijousGit <elimullenix4567@outlook.com>
2022-02-15 16:50:35 -0800

    Added top/bottom talonSRX's, plugged in port #'s, made enabled, disabled, and toggle methods, and the motors should spin when enabled

--- src/main/java/com/pigmice/frc/robot/subsystems/Intake.java
+++ src/main/java/com/pigmice/frc/robot/subsystems/Intake.java
@@ -2,18 +2,57 @@ package com.pigmice.frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.pigmice.frc.robot.Constants;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
 
+    TalonSRX talonBottom, talonTop;
+    boolean enabled = false;
+    static double motorSpeed;
+
     /** Creates a new Intake. */
     public Intake() {
+        talonBottom = new TalonSRX(Constants.IntakeConfig.intakeBottomPort);
+        talonTop = new TalonSRX(Constants.IntakeConfig.intakeTopPort);
+        motorSpeed = Constants.IntakeConfig.intakeSpeed;
+    }
+
+    public void enable() {
+        setEnabled(true);
+    }
+
+    public void disable() {
+        setEnabled(false);
+    }
+
+    public void toggle() {
+        this.setEnabled(!this.enabled);
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 
     @Override
     public void periodic() {
+
         // This method will be called once per scheduler run
+
+        // Checks if the motor has been enabled and sets the speed (bottom motor)
+        if (this.enabled) {
+            talonBottom.set(ControlMode.PercentOutput, motorSpeed);
+        } else {
+            talonBottom.set(ControlMode.PercentOutput, 0.0);
+        }
+
+        // Same thing as above but for the top motor
+        if (this.enabled) {
+            talonTop.set(ControlMode.PercentOutput, motorSpeed);
+        } else {
+            talonTop.set(ControlMode.PercentOutput, 0.0);
+        }
     }
 
     @Override
