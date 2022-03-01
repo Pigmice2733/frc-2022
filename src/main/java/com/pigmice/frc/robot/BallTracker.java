86cf7f46e850490b80180c319e046f1ae3d799ad
var Cepheid <avhoi83@gmail.com>
2022-02-28 17:50:13 -0800

    revised ball-tracker with a DEQue and added a contains method

--- src/main/java/com/pigmice/frc/robot/BallTracker.java
+++ src/main/java/com/pigmice/frc/robot/BallTracker.java
@@ -1,49 +1,11 @@
 package com.pigmice.frc.robot;
 
-/* import java.util.LinkedList;
-import java.util.Queue; */
+import java.util.Deque;
+import java.util.ArrayDeque;
 
-/** this here class is to find where yo ballz at */
-public class BallTracker {
-    /*
-     * Queue<BallColor> balls = new LinkedList<>();
-     * 
-     * // set the first position to a color and shift everything else up
-     * public void newBallStored(BallColor color) {
-     * balls.add(color);
-     * }
-     * 
-     * // get rid of the second position and shift everything else up
-     * public void ballLaunched() {
-     * balls.remove();
-     * }
-     * 
-     * // return the color of ball in a spot. 0 is the next one to be shot, 1 is the
-     * // second one to be shot
-     * public BallColor getCompartment(int spotNumber) {
-     * if (spotNumber == 0) {
-     * return balls.peek();
-     * } else {
-     * return balls.element();
-     * }
-     * }
-     * 
-     * enum BallColor {
-     * RED,BLUE,NONE
-     * }
-     */
-    enum Colors {
-        RED, BLUE, NONE
-    }
-
-    Colors[] balls = {Colors.NONE, Colors.NONE};
-
-    public boolean isEmpty() {
-        if (balls[0] == Colors.NONE) {
-            return true;
-        }
-        else {return false;}
-    }
+//this here class is to find where yo ballz at
+public class BallTracker{
+    Deque<BallColor> balls = new ArrayDeque<BallColor>();
 
     public boolean isFull() {
         if (balls[1] == Colors.NONE) {
@@ -64,18 +26,28 @@ public class BallTracker {
     }
 
     public void ballLaunched() {
-        balls[0] = Colors.NONE;
-        updateArray();
+        balls.removeFirst();
     }
 
-    public void updateArray() {
-        if (balls[0] == Colors.NONE) {
-            balls[0] = balls[1];
-            balls[1] = Colors.NONE;
+    // return the color of ball in a spot. 0 is the next one to be shot, 1 is the
+    // second one to be shot
+    public BallColor getCompartment(int spotNumber) {
+        if (spotNumber == 0) {
+            return balls.getFirst();
+        } else {
+            if(balls.size() == 2){
+                return balls.getLast();
+            } else {
+                return BallColor.NONE;
+            }
         }
     }
 
-    public Colors getColor(int spot) {
-        return balls[spot];
+    public boolean holdingColor(BallColor color){
+        return balls.contains(color);
+    }
+
+    public enum BallColor {
+        RED,BLUE,NONE
     }
 }
